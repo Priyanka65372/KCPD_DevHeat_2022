a69b193a9b32ddcdcb371b0033f005565ec99a65
Mala.NeerajSrinivas <ui21cs35@iiitsurat.ac.in>
2022-11-20 02:54:42 +0530

    completed

---
+++ footer.php
@@ -0,0 +1,102 @@
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h3>F2FMART</h3>
+            <p>
+              Devarakonda <br>
+              Nalgonda - 508248<br>
+              Telangana, India <br><br>
+              <strong>Phone:</strong> +91-7032928826<br>
+              <strong>Phone:</strong> +91-8247302611<br>
+              <strong>Email:</strong> info@f2fmart.com<br>
+            </p>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="displayarticles.php?articletype=Blog">Blog</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="displayarticles.php?articletype=News">News</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="displayproducts.php">Farmer's Kit</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="displaysales.php">Farmer's Market</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="index.php">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="aboutus.php">About</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="displayworkers.php">Hire</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="contact.php">Contact</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-newsletter">
+            <h4>Staff Login</h4>
+            <p>This feature is Available only for Staff or Administrator</p>
+            <button class="btn btn-success" type="submit" onclick="window.location='adminloginpanel.php';">Click here to Login</button>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container d-md-flex py-4">
+
+      <div class="mr-md-auto text-center text-md-left">
+        <div class="copyright">
+          &copy; Copyright <strong><span>F2FMART</span></strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+          <!-- All the links in the footer should remain intact. -->
+          <!-- You can delete the links only if you purchased the pro version. -->
+          <!-- Licensing information: https://bootstrapmade.com/license/ -->
+          <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/bethany-free-onepage-bootstrap-theme/ -->
+          Designed by KCPD TEAM...
+        </div>
+      </div>
+      <div class="social-links text-center text-md-right pt-3 pt-md-0">
+        <a href="https://twitter.com/Surender2423?t=VoQNym4sNmghx8Z3wFQfmQ&s=08" target="_blank" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a href="https://www.facebook.com/neeraj.srinivas.908" target="_blank" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="https://instagram.com/surendhar__rathod?igshid=MTg0ZDhmNDA=" target="_blank" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <!-- <a href="#" target="_blank" class="google-plus"><i class="bx bxl-skype"></i></a> -->
+        <a href="https://www.linkedin.com/in/neeraj-srinivas-26217a228/" target="_blank" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+  
+  <script src="assets/js/jquery.dataTables.min.js"></script>
+  <link href="assets/js/dataTables.responsive.min.js" rel="stylesheet">
+  
+<script>
+$(document).ready( function () {
+    $('#datatable').DataTable();
+	
+} );
+</script>
+</body>
+
+</html>
